a9ce9d0eb1274603082addb15c3579159aabc1f0
inc2734 <inc@2inc.org>
2022-11-21 13:35:58 +0900

    Some fixes

--- unitone-bbpress-integrator.php
+++ unitone-bbpress-integrator.php
@@ -37,8 +37,6 @@ class Bootstrap {
 	public function _bootstrap() {
 		load_plugin_textdomain( 'unitone-bbpress-integrator', false, basename( __DIR__ ) . '/languages' );
 
-		require UNITONE_BBPRESS_INTEGRATOR_PATH . '/inc/updater.php';
-
 		$theme = wp_get_theme( get_template() );
 		if ( 'unitone' !== $theme->template ) {
 			add_action(
@@ -55,6 +53,8 @@ class Bootstrap {
 			);
 			return;
 		}
+
+		require UNITONE_BBPRESS_INTEGRATOR_PATH . '/inc/updater.php';
 
 		if ( ! class_exists( 'bbPress' ) ) {
 			add_action(
@@ -84,3 +84,34 @@ class Bootstrap {
 
 require_once( __DIR__ . '/vendor/autoload.php' );
 new \UnitoneBbpressIntegrator\Bootstrap();
+
+
+
+
+add_action( 'admin_print_styles', function() {
+    if ( get_current_screen()->is_block_editor() ) {
+        do_action( 'admin_print_styles-widgets.php' );
+    }
+} );
+add_action( 'admin_print_scripts', function() {
+    if ( get_current_screen()->is_block_editor() ) {
+        do_action( 'load-widgets.php' );
+        do_action( 'widgets.php' );
+        do_action( 'sidebar_admin_setup' );
+        do_action( 'admin_print_scripts-widgets.php' );
+    }
+} );
+add_action( 'admin_print_footer_scripts', function() {
+    if ( get_current_screen()->is_block_editor() ) {
+        do_action( 'admin_print_footer_scripts-widgets.php' );
+    }
+} );
+add_action( 'admin_footer', function() {
+    if ( get_current_screen()->is_block_editor() ) {
+        do_action( 'admin_footer-widgets.php' );
+    }
+} );
+add_action( 'enqueue_block_editor_assets', function() {
+    wp_enqueue_script( 'wp-widgets' );
+    wp_add_inline_script( 'wp-widgets', 'wp.widgets.registerLegacyWidgetBlock()' );
+} );
