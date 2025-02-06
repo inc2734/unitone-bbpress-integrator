a714d2fe2a17658003dff4e5262a6a3315461328
inc2734 <inc@2inc.org>
2025-02-06 14:33:57 +0900

    Update updater

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,17 @@
 const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
 
-const plugins = [ ...defaultConfig.plugins ];
-plugins.splice(1, 1); //delete plugins.CleanWebpackPlugin
+if ( defaultConfig?.plugins ) {
+	const plugins = [ ...defaultConfig.plugins ];
 
-module.exports = {
-	...defaultConfig,
-	devtool: false,
-	mode: 'production',
-	plugins,
-};
+	plugins.splice(1, 1); //delete plugins.CleanWebpackPlugin
+	plugins.splice(2, 1); //delete plugins.CopyWebpackPlugin
+
+	module.exports = {
+		...defaultConfig,
+		// devtool: false,
+		// mode: process.env.NODE_ENV,
+		plugins,
+	};
+} else {
+	module.exports = defaultConfig;
+}
